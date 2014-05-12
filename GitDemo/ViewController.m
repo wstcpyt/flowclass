5422629180af1ddd5e7c23a337792732376ea7c1
yutong pang <yutongpang@yutongs-iMac.local>
2014-05-12 18:03:20 -0400

    A property for the sum was declared. The first addition was made and logged to the debugger.

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic) int sum;
 @end
 
 @implementation ViewController
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    int a=5;
+    int b=10;
+    self.sum=a+b;
+    NSLog(@"The result is:%d",self.sum);
 }
 
 - (void)didReceiveMemoryWarning
